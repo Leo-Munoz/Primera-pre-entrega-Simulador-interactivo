c502e65f32bb9d33222233faef26ffa2ef797155
Leo-Munoz <109494588+Leo-Munoz@users.noreply.github.com>
2022-11-14 23:25:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ if (videosPropios === true) {
 let musica1 = confirm("Quieres que tu video tenga música?")
 if (musica1 === true) {
     musica1 = 300;
-    let tipoDeMusica = prompt("Solo por curiosidad, ¿Qué tipo de música quieres en tu video?:\n Ingresa 1 - SENTIMENTAL\n Ingresa 2 - FOLK\n Ingresa 3 - EPICA\n Ingresa 0 - OTRA");
-    while (tipoDeMusica != "no") {
+    let tipoDeMusica = prompt("Solo por curiosidad, ¿Qué tipo de música quieres en tu video?:\n Ingresa 1 - SENTIMENTAL\n Ingresa 2 - FOLK\n Ingresa 3 - EPICA\n Ingresa 0 - OTRA").toUpperCase();
+    while (tipoDeMusica != "NO") {
         switch (tipoDeMusica) {
             case "0":
                 prompt("Que estilo de música tienes en mente?");
@@ -43,45 +43,51 @@ if (musica1 === true) {
                 alert("Debes ingresar una de las opciones.");
                 break;
         }
-        tipoDeMusica = prompt("¿Quieres otro tipo de música en tu video? Si escribes no, continuamos:\n Ingresa 1 - SENTIMENTAL\n Ingresa 2 - FOLK\n Ingresa 3 - EPICA\n Ingresa 0 - OTRA")
+        tipoDeMusica = prompt("¿Quieres otro tipo de música en tu video? Si escribes no, continuamos:\n Ingresa 1 - SENTIMENTAL\n Ingresa 2 - FOLK\n Ingresa 3 - EPICA\n Ingresa 0 - OTRA").toUpperCase();
     }
-    alert("Perfecto, tendrá música.");
 }
 
 else if (musica1 === false) {
-    let musica1 = confirm("Seguro? Te sugiero que le agregues música")
+    musica1 = confirm("Seguro? Te sugiero que le agregues música");
     if (musica1 === true) {
         musica1 = 300;
-        let tipoDeMusica = Number(prompt("Solo por curiosidad, ¿Qué tipo de música quieres en tu video?:\n Ingresa 1 - SENTIMENTAL\n Ingresa 2 - FOLK\n Ingresa 3 - EPICA\n Ingresa 0 - OTRA"))
-        while (tipoDeMusica != "0") {
+        let tipoDeMusica = prompt("Solo por curiosidad, ¿Qué tipo de música quieres en tu video?:\n Ingresa 1 - SENTIMENTAL\n Ingresa 2 - FOLK\n Ingresa 3 - EPICA\n Ingresa 0 - OTRA").toUpperCase();
+        while (tipoDeMusica != "NO") {
             switch (tipoDeMusica) {
-                case 1:
-                    alert("Ok, nos pondremos sentimentales.")
+                case "0":
+                    prompt("Que estilo de música tienes en mente?");
                     break;
-                case 2:
-                    alert("Ok, que suenen las guitarras acústicas.")
+                case "1":
+                    alert("Ok, nos pondremos sentimentales.");
                     break;
-                case 3:
-                    alert("Ok, llamen a Hans Zimmer.")
+                case "2":
+                    alert("Ok, que suenen las guitarras acústicas.");
+                    break;
+                case "3":
+                    alert("Ok, llamen a Hans Zimmer.");
                     break;
                 default:
-                    alert("Debes ingresar una de las opciones")
+                    alert("Debes ingresar una de las opciones.");
                     break;
             }
-            musica1 = 0;
-            let otraMusica = prompt("Que estilo de música tienes en mente?")
+            tipoDeMusica = prompt("¿Quieres otro tipo de música en tu video? Si escribes no, continuamos:\n Ingresa 1 - SENTIMENTAL\n Ingresa 2 - FOLK\n Ingresa 3 - EPICA\n Ingresa 0 - OTRA").toUpperCase();
         }
-    }
+    } else { musica1 = 0; }
 }
 
-let duracion = Number(prompt("Cuantos minutos crees que durará tu video? \n (debes ingresar un numero)"));
+let duracion = Number(prompt("Cuantos minutos crees que durará tu video?\n(debes ingresar un numero)"));
 
-while (duracion <= 0) {
-    alert("Debes elegir al menos un minuto.");
-    duracion = Number(prompt("Cuantos minutos crees que durará tu video? \n (debes ingresar un numero)"));
+function containsNumbers(str) {
+    return /\d/.test(str);
 }
-alert(`Recibido, tu video durará aproximadamente ${duracion} minutos.`);
-resultado = 0;
+
+while ((containsNumbers(duracion) === false) || duracion <= 0) {
+    alert("Debes elegir al menos un minuto.")
+    duracion = Number(prompt("Cuantos minutos crees que durará tu video?\n(debes ingresar un numero)"));
+}
+alert("Recibido, tu video durará aproximadamente " + duracion * 60 + " segundos.");
+
+let resultado = 0;
 
 function calculo(subtitulos, videosPropios, musica1, duracion) {
     resultado = (subtitulos + videosPropios + musica1) * duracion;
